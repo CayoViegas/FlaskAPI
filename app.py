228f9c080f5c954e9ecbb2a1667109504e9e387e
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2023-04-21 22:00:28 -0300

    entidades adicionadas

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask_pydantic_spec import FlaskPydanticSpec, Response
+from pydantic import BaseModel
 
 server = Flask(__name__)
+spec = FlaskPydanticSpec("Flask", title="FlaskAPI")
+spec.register(server)
+
+class Endereco(BaseModel):
+    id: int
+    logradouro: str
+    cep: str
+    numero: int
+    cidade: str
+
+class Pessoa(BaseModel):
+    id: int
+    nome: str
+    data_nascimento: str
+    endereco: Endereco
 
 @server.get("/pessoas")
+@spec.validate(resp=Response(HTTP_200=Pessoa))
 def get_pessoas():
     return "Pessoas"
+
+server.run()
