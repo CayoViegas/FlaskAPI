eb33badbb9cf44e0d106a4b72cf14edb9f85ec25
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2023-04-25 02:32:59 -0300

    abordagem alterada, post adicionado

--- app.py
+++ app.py
@@ -1,41 +1,54 @@
-from flask import Flask
-from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
-from pydantic import BaseModel
+import os
 import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request
 
-con = psycopg2.connect(host="localhost",
-                       database="flaskapi", 
-                       user="asd", 
-                       password="123")
+CREATE_ENDERECOS_TABLE = """CREATE TABLE IF NOT EXISTS Enderecos (
+                               id SERIAL PRIMARY KEY,
+                               logradouro VARCHAR(100) NOT NULL,
+                               cep CHAR(8) NOT NULL,
+                               numero INTEGER NOT NULL,
+                               cidade VARCHAR(100) NOT NULL
+                            );"""
 
-cur = con.cursor()
+CREATE_PESSOAS_TABLE = """CREATE TABLE IF NOT EXISTS Pessoas (
+                             id SERIAL PRIMARY KEY,
+                             nome VARCHAR(100) NOT NULL,
+                             data_nascimento DATE NOT NULL,
+                             endereco_id INTEGER REFERENCES Enderecos(id)
+                          );"""
 
-server = Flask(__name__)
-spec = FlaskPydanticSpec("Flask", title="FlaskAPI")
-spec.register(server)
+INSERT_PESSOA = """INSERT INTO Pessoas (nome, data_nascimento, endereco_id)
+                   VALUES (%s, %s, %s)
+                   RETURNING id;"""
 
-class Endereco(BaseModel):
-    id: int
-    logradouro: str
-    cep: str
-    numero: int
-    cidade: str
+INSERT_ENDERECO = """INSERT INTO Enderecos (logradouro, cep, numero, cidade)
+                     VALUES (%s, %s, %s, %s)
+                     RETURNING id;"""
+                             
+load_dotenv()
 
-class Pessoa(BaseModel):
-    id: int
-    nome: str
-    data_nascimento: str
-    endereco: Endereco
+app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
 
-@server.get("/pessoas")
-def get_pessoas():
-    return "Pessoas"
-
-@server.post("/pessoas")
-@spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
+@app.post("/pessoa")
 def criar_pessoa():
-    body = request.context.body.dict()
-    return body
+    data = request.get_json()
+    nome = data["nome"]
+    data_nascimento = data["data_nascimento"]
+    logradouro = data["logradouro"]
+    cep = data["cep"]
+    numero = data["numero"]
+    cidade = data["cidade"]
 
-
-server.run()
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_ENDERECOS_TABLE)
+            cursor.execute(CREATE_PESSOAS_TABLE)
+            cursor.execute(INSERT_ENDERECO, (logradouro, cep, numero, cidade))
+            id_endereco = cursor.fetchone()[0]
+            cursor.execute(INSERT_PESSOA, (nome, data_nascimento, id_endereco))
+            id_pessoa = cursor.fetchone()[0]
+    
+    return {"id": id_pessoa, "message": "Pessoa criada com sucesso."}, 201
